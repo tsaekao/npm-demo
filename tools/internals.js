4eba45b60006a0d4569540af47491a02f4ef272a
Stefan Cameron <stefan@stefcameron.com>
2020-05-25 11:18:18 -0500

    Update all dependencies

--- tools/internals.js
+++ tools/internals.js
@@ -1,5 +1,7 @@
 ////// Manual Node.js testing of rtv internals
 
+/* eslint-disable node/no-unsupported-features/es-syntax */
+
 import rtv from '../src/rtv';
 import impl from '../src/lib/impl';
 import * as types from '../src/lib/types';
