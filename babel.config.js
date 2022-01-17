eb7617d055e3cdd079364efc79fba90c7b6f6c9d
Stefan Cameron <stefan@stefcameron.com>
2022-01-17 11:25:05 -0600

    Update all deps including eslint

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   // Options: https://babeljs.io/docs/en/babel-preset-env#options
 
-  // environment specified via BABEL_ENV env var
+  // NOTE: VSCode and other IDEs will use whatever is in the root/base configuration
+  //  since they don't know (at least not by default) about any specific env to use
+
+  // when BABEL_ENV='<name>', that environment is merge on top of the root/base config
+  // @see https://babeljs.io/docs/en/options#env
+  // @see https://babeljs.io/docs/en/options#merging
   env: {
     build: {
       // @see tools/build/rollup-builds.js#getBabelConfig()
